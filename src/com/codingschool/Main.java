6e459ec7c3372eb7aff24d7d81dd28255e097f6c
bierzan@gmail.com <bierzan@gmail.com>
2018-12-06 23:19:45 +0100

    base

---
+++ src/com/codingschool/Main.java
@@ -0,0 +1,16 @@
+package com.codingschool;
+
+import com.codingschool.db.DatabaseConnection;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        // TODO zrobić sprawdzanie czy main leci z argumentami czy bez
+        //jeśli z argumentami to leci jako user
+        //jesli bez to jako admin
+
+    }
+}
---
+++ src/com/codingschool/model/User.java
@@ -0,0 +1,12 @@
+package com.codingschool.model;
+
+import java.math.BigInteger;
+
+public class User {
+
+    private String username;
+    private String password;
+    private String email;
+    private BigInteger id;
+//    private Group group;
+}
