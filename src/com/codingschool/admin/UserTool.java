4cdb32ba274df9f4ad6965657350dd4987ad4fa1
bierzan@gmail.com <bierzan@gmail.com>
2018-12-13 22:21:42 +0100

    editUser applied

--- src/com/codingschool/admin/UserTool.java
+++ src/com/codingschool/admin/UserTool.java
@@ -14,10 +14,12 @@ public class UserTool {
         String task = " ";
 
         try (Connection conn = DatabaseConnection.getConnection()) {
-            while (!task.equalsIgnoreCase("quit")){
+            while (!task.equalsIgnoreCase("quit")) {
                 task = printAndGetTask(conn);
                 if (task.equalsIgnoreCase("add")) {
                     addUser(conn);
+                } else if (task.equalsIgnoreCase("edit")){
+                    editUser(conn);
                 }
             }
 
@@ -101,7 +103,6 @@ public class UserTool {
 
         while (!stop) {
             System.out.println("Podaj nr grupy do której chesz przypisać użytkownika");
-            //todo istniejaca grupa lub nowa grupa
             while (!sc.hasNextInt()) {
                 System.out.println("To nie jest nr Id");
                 sc.next();
@@ -138,15 +139,111 @@ public class UserTool {
             }
         }
 
-        try{
-            User user = new User (username, password, email, groupId);
+        try {
+            User user = new User(username, password, email, groupId);
             user.save(conn);
 
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
         System.out.println("Użytkownik został dodany\n");
+
+    }
+
+    private static void editUser(Connection conn) {
+        int userId = 0;
+
+        String username;
+        String password;
+        String email = " ";
+        int groupId = 0;
+
+        System.out.println("Podaj id użytkownika, którego chcesz edytować.");
+        userId = getIntFromScanner();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Podaj nową nazwę dla użytkownika");
+        username = sc.next();
+
+        System.out.println("Podaj nowy email użytkownika");
+        boolean uniqueTest = true;
+
+        while (uniqueTest) {
+            email = sc.next();
+            while (!email.matches("[_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.([a-zA-Z]{2,}){1}")) {
+                System.out.println("to nie jest adres e-mail");
+                email = sc.next();
+            }
+
+            try {
+                uniqueTest = doesEmailExist(conn, email);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            if (uniqueTest) {
+                System.out.println("Taki email już istnieje, podaj inny");
+            }
+        }
+
+        System.out.println("Podaj nowe hasło");
+        password = sc.next();
+
+        boolean stop = false;
+
+        while (!stop) {
+            System.out.println("Podaj nr grupy do której chesz przypisać użytkownika");
+            while (!sc.hasNextInt()) {
+                System.out.println("To nie jest nr Id");
+                sc.next();
+            }
+
+            groupId = sc.nextInt();
+
+            try {
+                if (!doesGroupExist(conn, groupId)) {
+                    System.out.println("Taka grupa nie istnieje w bazie danych.\nCzy chcesz dodać nową grupę? (T/N)");
+                    String answer = sc.next();
+                    while (!answer.toLowerCase().matches("t|n")) {
+                        System.out.println("Czy chcesz dodać nową grupę?" +
+                                "\nT - tak," +
+                                "\nN - nie");
+                        answer = sc.next();
+                    }
+
+                    if (answer.toLowerCase().equals("t")) {
+                        System.out.println("Podaj nazwę dla nowej grupy");
+                        String newGroupName = sc.next();
+                        UserGroup newGroup = new UserGroup(newGroupName);
+                        newGroup.save(conn);
+                        groupId = newGroup.getId();
+                        stop = true;
+                    }
+
+                } else {
+                    stop = true;
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            User userToEdit = User.loadById(conn, userId);
+            userToEdit.setUsername(username);
+            userToEdit.setEmail(email);
+            userToEdit.setPassword(password);
+            userToEdit.setGroup(groupId);
+
+            userToEdit.update(conn);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Użytkownik został edytowany\n");
 
     }
 
@@ -156,7 +253,7 @@ public class UserTool {
         PreparedStatement prepStm = conn.prepareStatement(sql);
         prepStm.setString(1, email);
         ResultSet rs = prepStm.executeQuery();
-        if (rs.next()){
+        if (rs.next()) {
             return true;
         } else {
             return false;
@@ -170,10 +267,20 @@ public class UserTool {
         PreparedStatement prepStm = conn.prepareStatement(sql);
         prepStm.setInt(1, groupId);
         ResultSet rs = prepStm.executeQuery();
-        if (rs.next()){
+        if (rs.next()) {
             return true;
         } else {
             return false;
         }
+    }
+
+    private static int getIntFromScanner()//todo mozna podmienic z resztą sprawdzen
+    {
+        Scanner scan = new Scanner(System.in);
+        while (!scan.hasNextInt()) {
+            System.out.println("To nie jest numer ID");
+            scan.next();
+        }
+        return scan.nextInt();
     }
 }
