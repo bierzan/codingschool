017068e633fa4b323d2081226be0e74ebaf9e1e4
bierzan@gmail.com <bierzan@gmail.com>
2018-12-13 22:07:05 +0100

    addUser applied

--- src/com/codingschool/admin/UserTool.java
+++ src/com/codingschool/admin/UserTool.java
@@ -1,6 +1,7 @@
 package com.codingschool.admin;
 
 import com.codingschool.db.DatabaseConnection;
+import com.codingschool.model.User;
 import com.codingschool.model.UserGroup;
 
 import java.sql.*;
@@ -10,10 +11,16 @@ import java.util.Scanner;
 public class UserTool {
     public static void main() {
 
+        String task = " ";
+
         try (Connection conn = DatabaseConnection.getConnection()) {
-            if (printAndGetTask(conn).equalsIgnoreCase("add")) {
-                addUser(conn);
+            while (!task.equalsIgnoreCase("quit")){
+                task = printAndGetTask(conn);
+                if (task.equalsIgnoreCase("add")) {
+                    addUser(conn);
+                }
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -61,8 +68,7 @@ public class UserTool {
         String username;
         String password;
         String email = " ";
-        String group;
-
+        int groupId = 0;
 
         System.out.println("Podaj nazwę użytkownika");
         username = sc.next();
@@ -78,7 +84,7 @@ public class UserTool {
             }
 
             try {
-                uniqueTest = doesItExist(conn, "email", email);
+                uniqueTest = doesEmailExist(conn, email);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -91,36 +97,83 @@ public class UserTool {
         System.out.println("Podaj hasło");
         password = sc.next();
 
-        System.out.println("Podaj nr grupy do której chesz przypisać użytkownika"); //todo istniejaca grupa lub nowa grupa
-        group = sc.next();
+        boolean stop = false;
 
-        System.out.println(username);
-        System.out.println(email);
-        System.out.println(password);
-        System.out.println(group);
+        while (!stop) {
+            System.out.println("Podaj nr grupy do której chesz przypisać użytkownika");
+            //todo istniejaca grupa lub nowa grupa
+            while (!sc.hasNextInt()) {
+                System.out.println("To nie jest nr Id");
+                sc.next();
+            }
+
+            groupId = sc.nextInt();
+
+            try {
+                if (!doesGroupExist(conn, groupId)) {
+                    System.out.println("Taka grupa nie istnieje w bazie danych.\nCzy chcesz dodać nową grupę? (T/N)");
+                    String answer = sc.next();
+                    while (!answer.toLowerCase().matches("t|n")) {
+                        System.out.println("Czy chcesz dodać nową grupę?" +
+                                "\nT - tak," +
+                                "\nN - nie");
+                        answer = sc.next();
+                    }
+
+                    if (answer.toLowerCase().equals("t")) {
+                        System.out.println("Podaj nazwę dla nowej grupy");
+                        String newGroupName = sc.next();
+                        UserGroup newGroup = new UserGroup(newGroupName);
+                        newGroup.save(conn);
+                        groupId = newGroup.getId();
+                        stop = true;
+                    }
+
+                } else {
+                    stop = true;
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try{
+            User user = new User (username, password, email, groupId);
+            user.save(conn);
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Użytkownik został dodany\n");
 
     }
 
-    private static boolean doesItExist(Connection conn, String column, String record) throws SQLException {
+    private static boolean doesEmailExist(Connection conn, String email) throws SQLException {
 
-        String sql = "SELECT ? FROM users;";
+        String sql = "SELECT * FROM users WHERE email = ? ;";
         PreparedStatement prepStm = conn.prepareStatement(sql);
-        prepStm.setString(1, column);
+        prepStm.setString(1, email);
         ResultSet rs = prepStm.executeQuery();
-        ArrayList<String> records = new ArrayList<>();
-        boolean result = false;
-
-        while (rs.next()) {
-            records.add(rs.getString("email"));
+        if (rs.next()){
+            return true;
+        } else {
+            return false;
         }
+    }
 
-        for (String mail : records) {
-            if (mail.equals(record)) {
-                result = true;
-                return result;
-            }
+
+    private static boolean doesGroupExist(Connection conn, int groupId) throws SQLException {
+
+        String sql = "SELECT * FROM users WHERE user_group_id = ? ;";
+        PreparedStatement prepStm = conn.prepareStatement(sql);
+        prepStm.setInt(1, groupId);
+        ResultSet rs = prepStm.executeQuery();
+        if (rs.next()){
+            return true;
+        } else {
+            return false;
         }
-        return result;
-
     }
 }
